e869a0856c462bc95038b673f7423373fb60f76a
Daman <damanpreet@web.de>
2018-06-20 11:24:54 +0200

    weitere Klassen

--- src/games/gsClever/Logic/Orange.java
+++ src/games/gsClever/Logic/Orange.java
@@ -2,4 +2,9 @@ package games.gsClever.Logic;
 
 public class Orange {
 
+	private int [] fields = new int [11];
+	
+	public void enterNumbers(int number) {
+		////Switch
+	}
 }
--- src/games/gsClever/Logic/Purple.java
+++ src/games/gsClever/Logic/Purple.java
@@ -2,4 +2,10 @@ package games.gsClever.Logic;
 
 public class Purple {
 
+	
+	private int [] fields = new int [11];
+	
+	public void enterNumbers(int number) {
+		////Switch
+	}
 }
