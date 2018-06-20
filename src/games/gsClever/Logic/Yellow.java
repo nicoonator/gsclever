5355f2068d78ffa80ceb55f98de2ab2e1af17c21
omer2 <omer2@10.7.249.135>
2018-06-20 11:27:24 +0200

    yellow

--- src/games/gsClever/Logic/Yellow.java
+++ src/games/gsClever/Logic/Yellow.java
@@ -5,6 +5,25 @@ public class Yellow {
 private	boolean [] fields = new boolean [12];
 
 
+public Yellow (boolean[] fields) {
+	
+	this.fields= fields;
+}
+
+
+public boolean[] getFields(){
+	return fields;
+	
+	
+}
+
+public void setFields(){
+	
+	this.fields=fields;
+	
+	
+}
+
 
 
 public void enterCross( int fieldId ) {
