780e42d29cc2d2b59aa63e1fa520bd69a0469187
Joel Wolf <joel.wolf@student.uni-siegen.de>
2018-06-20 11:54:25 +0200

    Added Management.java

---
+++ src/games/gsClever/Logic/Management.java
@@ -0,0 +1,26 @@
+package games.gsClever.Logic;
+
+public class Management {
+
+	private int diceRepeatCount;
+	private int diceRepeatUsed;
+	private int additionalDiceCount;
+	private int additionalDiceUsed;
+	
+	public int determinePoints() {
+		
+		//TODO
+		
+		return 0;
+	}
+	
+	public void useDiceRepeat() {
+		
+		//TODO
+	}
+	
+	public void useAdditionalDice() {
+		
+		//TODO
+	}
+}
