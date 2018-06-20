83a4a506b7bc167537ca02280feb3335ff1a2305
Daman <damanpreet@web.de>
2018-06-20 12:03:49 +0200

    die Klasse Orange

--- src/games/gsClever/Logic/Orange.java
+++ src/games/gsClever/Logic/Orange.java
@@ -3,8 +3,38 @@ package games.gsClever.Logic;
 public class Orange {
 
 	private int [] fields = new int [11];
-	
-	public void enterNumbers(int number) {
+	private int fieldCount =0 ;
+	public specialEvent  enterNumbers(int number) {
 		////Switch
+		fieldCount++;
+		
+		switch(fieldCount-1) {
+		case 0: fields[0] = number;
+				break;
+		case 1: fields[1] = number;
+				break;
+		case 2: fields[2] = number;
+				
+				break;
+		case 3: fields[3] = number*2;
+				break;
+		case 4: fields[4] = number;
+				return specialEvent.diceRepeat;	
+		case 5: fields[5] = number;
+				return specialEvent.enterCrossYellow;
+		case 6: fields[6]= number*2;
+				break;
+		case 7: fields[7] = number;
+				break;
+		case 8 : fields[8] = number*2;
+				break;
+		case 9: fields[9] = number;
+				
+		case 10: fields[10] = number*3;
+				break;
+				
+		}
+		return null;
+		
 	}
 }
