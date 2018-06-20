3a464f8b53d241bd2e522fd1d52ffe2c88fc2b51
omer2 <omer2@10.7.249.135>
2018-06-20 12:30:33 +0200

    klassen update

--- src/games/gsClever/Logic/Blue.java
+++ src/games/gsClever/Logic/Blue.java
@@ -1,5 +1,29 @@
 package games.gsClever.Logic;
 
 public class Blue {
+	private	boolean [] fields = new boolean [12];
 
+
+	public Blue (boolean[] fields) {
+		
+		this.fields= fields;
+	}
+
+
+	public boolean[] getFields(){
+		return fields;
+
+			
+	}
+
+
+
+	public void enterCross( int fieldId ) {
+		
+		fields [fieldId] = true ;
+		
+		
+	}
+
+		
 }
