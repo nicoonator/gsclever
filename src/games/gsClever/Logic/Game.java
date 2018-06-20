1058a6625a631fab2dc48a7ca700c6ac1c777619
Joel Wolf <joel.wolf@student.uni-siegen.de>
2018-06-20 11:37:15 +0200

    Updated Game.java

--- src/games/gsClever/Logic/Game.java
+++ src/games/gsClever/Logic/Game.java
@@ -1,5 +1,47 @@
 package games.gsClever.Logic;
 
+import java.util.List;
+
 public class Game {
 
+	private int round;
+	private int playerCount;
+	
+	public int getRound() {
+		return round;
+	}
+	
+	public int getPlayerCount() {
+		return playerCount;
+	}
+	
+	public void setPlayerCount(int playerCount) {
+		this.playerCount = playerCount;
+	}
+	
+	public boolean startRound() {
+		
+		//TODO
+		
+		return false;
+	}
+	
+	public List<Dice> rollDices(List<Dice> dices) {
+		
+		//TODO
+		
+		return dices;
+	}
+	
+	public int investigateWinner() {
+		
+		//TODO
+		
+		return 0;
+	}
+	
+	public void clearTray() {
+		
+		//TODO
+	}
 }
