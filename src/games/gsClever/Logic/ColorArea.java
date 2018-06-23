640182babe83b2370478dd4afd3d709eb178d912
Joel Wolf <joel.wolf@student.uni-siegen.de>
2018-06-23 22:48:29 +0200

    Added ColorArea.java

---
+++ src/games/gsClever/Logic/ColorArea.java
@@ -0,0 +1,19 @@
+package games.gsClever.Logic;
+
+public abstract class ColorArea {
+
+	public boolean[] isClickable() {
+		
+		return null;
+	}
+	
+	public boolean getFox() {
+		
+		return false;
+	}
+	
+	public int determinePoints() {
+		
+		return 0;
+	}
+}
