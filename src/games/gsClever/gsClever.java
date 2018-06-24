f7d27a2dfce7a28138d326e8f92d3e88f10daa98
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-06-24 15:12:16 +0200

    added empty methods to gsClever Class

--- src/games/gsClever/gsClever.java
+++ src/games/gsClever/gsClever.java
@@ -1,12 +1,100 @@
-/**
- * 
- */
 package games.gsClever;
 
+import java.util.ArrayList;
+
+import gameClasses.Game;
+import gameClasses.GameState;
+import userManagement.User;
+
 /**
+ * Game Class which get accessed by server
  * @author Nico Rychlik
  *
  */
-public class gsClever {
+public class gsClever extends Game {
+
+	@Override
+	public String getSite() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCSS() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getJavaScript() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMaxPlayerAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCurrentPlayerAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void execute(User user, String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<User> getPlayerList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<User> getSpectatorList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGameData(String eventName, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addSpectator(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isJoinable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void playerLeft(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GameState getGameState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
