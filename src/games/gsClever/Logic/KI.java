0246d1f02b9669965e843e4fb043a58f80919e31
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-06-27 19:30:19 +0200

    KI Template

---
+++ src/games/gsClever/Logic/KI.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package games.gsClever.Logic;
+
+import userManagement.User;
+
+/**
+ * @author Nico Rychlik
+ *
+ */
+public class KI extends User {
+
+	/**
+	 * @param name
+	 * @param pw
+	 */
+	public KI(String name, String pw) {
+		super(name, pw);
+		// TODO Auto-generated constructor stub
+	}
+
+}
