005247453244a3a1a65e774bc1afd64940817b98
Joel Wolf <joel.wolf@student.uni-siegen.de>
2018-07-03 21:46:07 +0200

    Updated Return.java

--- src/games/gsClever/Logic/Return.java
+++ src/games/gsClever/Logic/Return.java
@@ -25,20 +25,20 @@ public class Return {
 		return round;
 	}
 
-	public Matchfield getMatchfield(int index) {
-		return matchfield[index];
+	public Matchfield getMatchfield(int playerId) {
+		return matchfield[playerId];
 	}
 
-	public void setMatchfield(Matchfield matchfield, int index) {
-		this.matchfield[index] = matchfield;
+	public void setMatchfield(Matchfield matchfield, int playerId) {
+		this.matchfield[playerId] = matchfield;
 	}
 
-	public IsClickable getClickable(int index) {
-		return clickable[index];
+	public IsClickable getClickable(int playerId) {
+		return clickable[playerId];
 	}
 
-	public void setClickable(IsClickable clickable, int index) {
-		this.clickable[index] = clickable;
+	public void setClickable(IsClickable clickable, int playerId) {
+		this.clickable[playerId] = clickable;
 	}
 
 	public Dice[] getDices() {
