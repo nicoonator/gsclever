ec5062983612b2667209530c2091a3a72fe03b61
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-07-04 13:53:25 +0200

    Hab Angefangen eine Array lese Funktion zu schreiben
    erste Funkitonen bereits implementiert

--- gsClever/javascript/gsClever.js
+++ gsClever/javascript/gsClever.js
@@ -3,50 +3,59 @@ addListener('standardEvent', function(event) {
     var arr = stringFromServer.split(',');
     // console.log(arr);
 
-    if(arr.length==11){
-        for(var i=0; i<9; i++) { arrFields[i] = +arr[i]; }
-        playerMessage = arr[9];
-        var str = arr[10];
-        if(str=="HOST"){
-            console.log(arr[10]);
-            setVisible();
-        }
+    if (arr.length == 11) {
+	for (var i = 0; i < 9; i++) {
+	    arrFields[i] = +arr[i];
+	}
+	playerMessage = arr[9];
+	var str = arr[10];
+	if (str == "HOST") {
+	    console.log(arr[10]);
+	    setVisible();
+	}
 
-
-
-        sentFields = [0,0,0,0,0,0,0,0,0];
-        document.getElementById("Player").innerHTML = playerMessage;
-        redraw();
+	sentFields = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+	document.getElementById("Player").innerHTML = playerMessage;
+	redraw();
     }
     statusWait = false;
 });
-addListener('START', function(event){
-// TODO
+addListener('START', function(event) {
+    // TODO
 });
-addListener('PLAYERLEFT', function(event){
+addListener('PLAYERLEFT', function(event) {
     var stringFromServer = event.data;
     playerMessage = stringFromServer;
     document.getElementById("Player").innerHTML = playerMessage;
 });
-addListener('CLOSE', function(event){
-    document.getElementById("Player").innerHTML = "Spiel wurde vom Host beendet!";
-});
-addListener('TESTWUERFELN', function(event){
+addListener(
+	'CLOSE',
+	function(event) {
+	    document.getElementById("Player").innerHTML = "Spiel wurde vom Host beendet!";
+	});
+addListener('TESTWUERFELN', function(event) {
     var stringFromServer = event.data;
     var arr = stringFromServer.split(',');
     console.log(stringFromServer);
     console.log(arr);
-    for(var i=0; i<6; i++) {
-	number[i] = +arr[i]; 
-	}
+    for (var i = 0; i < 6; i++) {
+	number[i] = +arr[i];
+    }
     // var number = Number(stringFromServer);
     // Hier wird ein Array uebergeben:
-    
+
     setDice(number);
 });
 
-var number = [0,0,0,0,0,0];
+var playerAmount = 2;
+var number = [ 0, 0, 0, 0, 0, 0 ];
 playerMessage = "";
+var BlauWert = 0;
+var GelbWert = 0;
+var OrangeWert = 0;
+var LilaWert = 0;
+var GreenWert = 0;
+var WeissWert = 0;
 var Blau1 = "/gsClever/Images/Dice/1BLAU.jpg";
 var Blau2 = "/gsClever/Images/Dice/2BLAU.jpg";
 var Blau3 = "/gsClever/Images/Dice/3BLAU.jpg";
@@ -84,157 +93,206 @@ var White4 = "/gsClever/Images/Dice/4WHITE.jpg";
 var White5 = "/gsClever/Images/Dice/5WHITE.jpg";
 var White6 = "/gsClever/Images/Dice/6WHITE.jpg";
 // TODO
-var img1 ="/gsClever/Images/Numbers/1.png";
-var img2 ="/gsClever/Images/Numbers/2.png";
-var img3 ="/gsClever/Images/Numbers/3.png";
-var img4 ="/gsClever/Images/Numbers/4.png";
-var img5 ="/gsClever/Images/Numbers/5.png";
-var img6 ="/gsClever/Images/Numbers/6.png";
-var img8 ="/gsClever/Images/Numbers/8.png";
-var img9 ="/gsClever/Images/Numbers/9.png";
-var img12 ="/gsClever/Images/Numbers/12.png";
-var img15 ="/gsClever/Images/Numbers/15.png";
-var img18 ="/gsClever/Images/Numbers/18.png";
+var img1 = "/gsClever/Images/Numbers/1.png";
+var img2 = "/gsClever/Images/Numbers/2.png";
+var img3 = "/gsClever/Images/Numbers/3.png";
+var img4 = "/gsClever/Images/Numbers/4.png";
+var img5 = "/gsClever/Images/Numbers/5.png";
+var img6 = "/gsClever/Images/Numbers/6.png";
+var img8 = "/gsClever/Images/Numbers/8.png";
+var img9 = "/gsClever/Images/Numbers/9.png";
+var img12 = "/gsClever/Images/Numbers/12.png";
+var img15 = "/gsClever/Images/Numbers/15.png";
+var img18 = "/gsClever/Images/Numbers/18.png";
 
 var ximg = "/gsClever/Images/X.png";
 var circleimg = "/gsClever/Images/circle.png";
 var statusWait = true;
 
-var arrFields = [0,0,0,0,0,0,0,0,0];
-var sentFields = [0,0,0,0,0,0,0,0,0];
+var arrFields = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+var sentFields = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
 var currentPlayer = 0;
 
-function setField(x){
-    sentFields[x]=1;
+function setField(x) {
+    sentFields[x] = 1;
 }
 
-function getDiceImg(x){
-    switch(x){
-        case 0: return
-        case 1: return Blau1
-        case 2: return Blau2
-        case 3: return Blau3
-        case 4: return Blau4
-        case 5: return Blau5
-        case 6: return Blau6
-        
-        case 7: return Gelb1
-        case 8: return Gelb2
-        case 9: return Gelb3
-        case 10: return Gelb4
-        case 11: return Gelb5
-        case 12: return Gelb6
-        
-        case 13: return Green1
-        case 14: return Green2
-        case 15: return Green3
-        case 16: return Green4
-        case 17: return Green5
-        case 18: return Green6
-        
-        case 19: return Lila1
-        case 20: return Lila2
-        case 21: return Lila3
-        case 22: return Lila4
-        case 23: return Lila5
-        case 24: return Lila6
-        
-        case 25: return Orange1
-        case 26: return Orange2
-        case 27: return Orange3
-        case 28: return Orange4
-        case 29: return Orange5
-        case 30: return Orange6
-        
-        case 31: return White1
-        case 32: return White2
-        case 33: return White3
-        case 34: return White4
-        case 35: return White5
-        case 36: return White6
-    }
-}
-function getImg(x){
-    switch(x){
-    case 0: return ximg
-    case 1: return circleimg
-    }
-}
-function getNumberImg(x){
-    switch(x){
-    case 1: return img1
-    case 2: return img2
-    case 3: return img3
-    case 4: return img4
-    case 5: return img5
-    case 6: return img6
-    case 8: return img8
-    case 9: return img9
-    case 12: return img12
-    case 15: return img15
-    case 18: return img18
-    }
-}
+function getDiceImg(x) {
+    switch (x) {
+    case 0:
+	return
+	
 
+    case 1:
+	return Blau1
+    case 2:
+	return Blau2
+    case 3:
+	return Blau3
+    case 4:
+	return Blau4
+    case 5:
+	return Blau5
+    case 6:
+	return Blau6
+
+    case 7:
+	return Gelb1
+    case 8:
+	return Gelb2
+    case 9:
+	return Gelb3
+    case 10:
+	return Gelb4
+    case 11:
+	return Gelb5
+    case 12:
+	return Gelb6
+
+    case 13:
+	return Green1
+    case 14:
+	return Green2
+    case 15:
+	return Green3
+    case 16:
+	return Green4
+    case 17:
+	return Green5
+    case 18:
+	return Green6
+
+    case 19:
+	return Lila1
+    case 20:
+	return Lila2
+    case 21:
+	return Lila3
+    case 22:
+	return Lila4
+    case 23:
+	return Lila5
+    case 24:
+	return Lila6
+
+    case 25:
+	return Orange1
+    case 26:
+	return Orange2
+    case 27:
+	return Orange3
+    case 28:
+	return Orange4
+    case 29:
+	return Orange5
+    case 30:
+	return Orange6
+
+    case 31:
+	return White1
+    case 32:
+	return White2
+    case 33:
+	return White3
+    case 34:
+	return White4
+    case 35:
+	return White5
+    case 36:
+	return White6
+    }
+}
+function getImg(x) {
+    switch (x) {
+    case 0:
+	return ximg
+    case 1:
+	return circleimg
+    }
+}
+function getNumberImg(x) {
+    switch (x) {
+    case 1:
+	return img1
+    case 2:
+	return img2
+    case 3:
+	return img3
+    case 4:
+	return img4
+    case 5:
+	return img5
+    case 6:
+	return img6
+    case 8:
+	return img8
+    case 9:
+	return img9
+    case 12:
+	return img12
+    case 15:
+	return img15
+    case 18:
+	return img18
+    }
+}
 
 /*
  * (window).on('load',function () { // TODO: Code goes here }
  */
 
-
-    
-    
-function updateGameState(){
+function updateGameState() {
     statusWait = true;
     sendDataToServer(sentFields);
 }
 
-function restart(){
+function restart() {
     statusWait = true;
     sendDataToServer("RESTART");
 }
-function setVisible(){
-    document.getElementById("restartButton").style.visibility ="visible";	
-    document.getElementById("closeButton").style.visibility ="visible";
+function setVisible() {
+    document.getElementById("restartButton").style.visibility = "visible";
+    document.getElementById("closeButton").style.visibility = "visible";
 }
-function closeGame(){
+function closeGame() {
     sendDataToServer("CLOSE");
 }
-function wuerfeln(){
+function wuerfeln() {
     sendDataToServer("WUERFELN");
 }
-function nachwuerfeln(){
+function nachwuerfeln() {
     sendDataToServer("NACHWUERFELN");
 }
-function zusatzwuerfeln(){
+function zusatzwuerfeln() {
     sendDataToServer("ZUSATZWUERFELN");
 }
-function showview(x){
-    switch(x){
-    	case 2: {
-    	    document.getElementById("spieler2").style.visibility ="visible";
-    	    document.getElementById("spieler3").style.visibility ="hidden";
-    	    document.getElementById("spieler4").style.visibility ="hidden";
-    	break;
-    	};
-    	case 3:	{
-    	    document.getElementById("spieler2").style.visibility ="hidden";
-    	    document.getElementById("spieler3").style.visibility ="visible";
-    	    document.getElementById("spieler4").style.visibility ="hidden";
-    	break;
-    	}
-    	case 4:	{
-    	    document.getElementById("spieler2").style.visibility ="hidden";
-    	    document.getElementById("spieler3").style.visibility ="hidden";
-    	    document.getElementById("spieler4").style.visibility ="visible";
-    	break;
-    	}
+function showview(x) {
+    switch (x) {
+    case 2: {
+	document.getElementById("spieler2").style.visibility = "visible";
+	document.getElementById("spieler3").style.visibility = "hidden";
+	document.getElementById("spieler4").style.visibility = "hidden";
+	break;
+    }
+	;
+    case 3: {
+	document.getElementById("spieler2").style.visibility = "hidden";
+	document.getElementById("spieler3").style.visibility = "visible";
+	document.getElementById("spieler4").style.visibility = "hidden";
+	break;
+    }
+    case 4: {
+	document.getElementById("spieler2").style.visibility = "hidden";
+	document.getElementById("spieler3").style.visibility = "hidden";
+	document.getElementById("spieler4").style.visibility = "visible";
+	break;
+    }
     }
 }
-function skip(){
+function skip() {
     sendDataToServer("SKIP");
 }
-function setDice(x){
+function setDice(x) {
     // Hier wird das Silberfeld zurueckgesetzt wenn die neuen Wuerfel geladen
     // werden
     document.getElementById("wuerfelfeldsilber1").src = "";
@@ -243,29 +301,128 @@ function setDice(x){
     document.getElementById("wuerfelfeldsilber4").src = "";
     document.getElementById("wuerfelfeldsilber5").src = "";
     document.getElementById("wuerfelfeldsilber6").src = "";
-    
-    for(var i=0; i<6; i++){
+
+    for (var i = 0; i < 6; i++) {
 	setDiceImg(x[i]);
     }
 }
 
-function setDiceImg(x){
-    if(x>0 && x<=6){
+function setDiceImg(x) {
+    if (x > 0 && x <= 6) {
 	document.getElementById("wuerfelfeldsilber3").src = getDiceImg(x);
-    } 
-    if(x>6 && x<=12){
+    }
+    if (x > 6 && x <= 12) {
 	document.getElementById("wuerfelfeldsilber2").src = getDiceImg(x);
-    } 
-    if(x>12 && x<=18){
+    }
+    if (x > 12 && x <= 18) {
 	document.getElementById("wuerfelfeldsilber6").src = getDiceImg(x);
-    } 
-    if(x>18 && x<=24){
+    }
+    if (x > 18 && x <= 24) {
 	document.getElementById("wuerfelfeldsilber5").src = getDiceImg(x);
-    } 
-    if(x>24 && x<=30){
+    }
+    if (x > 24 && x <= 30) {
 	document.getElementById("wuerfelfeldsilber4").src = getDiceImg(x);
-    } 
-    if(x>30 && x<=36){
+    }
+    if (x > 30 && x <= 36) {
 	document.getElementById("wuerfelfeldsilber1").src = getDiceImg(x);
-    } 
+    }
+}
+
+function setRound(x) {
+    var elements = document.getElementsByClassName('Rundefeld1');
+    for (var i = 0; i < elements.length; i++) {
+	elements[i].style.visibility = "visible";
+    }
+    if (x > 1) {
+	elements = document.getElementsByClassName('Rundefeld2');
+	for (var i = 0; i < elements.length; i++) {
+	    elements[i].style.visibility = "visible";
+	}
+    }
+    if (x > 2) {
+	elements = document.getElementsByClassName('Rundefeld3');
+	for (var i = 0; i < elements.length; i++) {
+	    elements[i].style.visibility = "visible";
+	}
+    }
+    if (x > 3) {
+	elements = document.getElementsByClassName('Rundefeld4');
+	for (var i = 0; i < elements.length; i++) {
+	    elements[i].style.visibility = "visible";
+	}
+    }
+    if (x > 4) {
+	elements = document.getElementsByClassName('Rundefeld5');
+	for (var i = 0; i < elements.length; i++) {
+	    elements[i].style.visibility = "visible";
+	}
+    }
+    if (x > 5) {
+	elements = document.getElementsByClassName('Rundefeld6');
+	for (var i = 0; i < elements.length; i++) {
+	    elements[i].style.visibility = "visible";
+	}
+    }
+}
+
+function extractArray(x) {
+    var limit = 345;
+    switch (PlayerAmount) {
+    case 2: {
+	limit = 180;
+	break;
+    }
+    case 3: {
+	limit = 263
+	break;
+    }
+    }
+    for (var i = 0; i < limit; i++) {
+	switch (i) {
+	case 0: {
+	    setRound(x[i]);
+	    break;
+	}
+	case 1: {
+	    currentplayer = x[i];
+	    break;
+	}
+	case 2: {
+	    BlauWert= x[i];
+	    break;
+	}
+	case 3: {
+	    GelbWert= x[i];
+	    break;
+	}
+	case 4: {
+	    OrangeWert= x[i];
+	    break;
+	}
+	case 5: {
+	    LilaWert= x[i];
+	    break;
+	}
+	case 6: {
+	    GreenWert= x[i];
+	    break;
+	}
+	case 7: {
+	    WeissWert= x[i];
+	    break;
+	}
+	case 8: {
+	    setDiceOnField(1,x[i]);
+	    break;
+	}
+	case 9: {
+	    setDiceOnField(2,x[i]);
+	    break;
+	}
+	case 10: {
+	    setDiceOnField(3,x[i]);
+	    break;
+	}
+	}
+    }
 }
