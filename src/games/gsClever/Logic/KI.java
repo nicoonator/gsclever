df808a3acddd206ff426d0f9b91a4db96c8d8702
Nico Rychlik <nico.rychlik1996@student.uni-siegen.de>
2018-07-12 13:13:04 +0200

    KI

--- src/games/gsClever/Logic/KI.java
+++ src/games/gsClever/Logic/KI.java
@@ -11,13 +11,35 @@ import userManagement.User;
  */
 public class KI extends User {
 
+	private Player player;
+	private Return returnback;
+	private int playerID;
+
 	/**
 	 * @param name
 	 * @param pw
 	 */
-	public KI(String name, String pw) {
-		super(name, pw);
-		// TODO Auto-generated constructor stub
+	public KI(int id) {
+		super(Integer.toString(id), "none");
+
 	}
 
+	public Return doSomething(MainLogic currentGame, Return returnBack2) {
+		playerID = currentGame.determinePlayerId(this.getName());
+		returnback = returnBack2;
+		while (returnback.getCurrentPlayer() == playerID) {
+			if (returnback.getClickable(playerID).isRollDices()) {
+				try {
+					returnback = currentGame.click(playerID, Area.rollDices, 0);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} /*
+				 * else if(//TODO){ //TODO }
+				 */
+
+		}
+		return returnback;
+	}
 }
